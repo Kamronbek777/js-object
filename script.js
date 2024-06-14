7a362b4837a3467be6207d7cb3c31f221adec8a7
Kamronbek777 <kamronbek25082010@gmail.com>
2024-06-15 00:01:07 +0500

    Version 1 | All rights reverved | Kamronbek 2024

---
+++ script.js
@@ -0,0 +1,11 @@
+let userName = prompt("Enter your name");
+let userId = +prompt("Enter your id");
+let userPassword = +prompt("Enter your password");
+
+let object = {
+    name: userName,
+    id: userId,
+    password: 123
+}
+
+console.log(object);
